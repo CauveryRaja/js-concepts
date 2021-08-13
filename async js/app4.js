cd3ed2032ce55c8e46407e163444dc6db012fd4e
CauveryRaja <cauveryraja25@gmail.com>
2021-08-13 07:45:20 +0530

    Practise Promise with timeout and interval

--- async js/app4.js
+++ async js/app4.js
@@ -1,18 +1,48 @@
-const timeoutId = setTimeout(function print(msg) {
-    console.log('Inside Timeout', msg);
-}, 3000, 'hello');
+// const timeoutId = setTimeout(function print(msg) {
+//     console.log('Inside Timeout', msg);
+// }, 3000, 'hello');
 
-console.log(timeoutId);
-// clearTimeout(id);
+// console.log(timeoutId);
+// // clearTimeout(id);
 
-const intervalId = setInterval(function print() {
-    console.log('Inside Interval');
-}, 1000);
+// const intervalId = setInterval(function print() {
+//     console.log('Inside Interval');
+// }, 1000);
 
-console.log(intervalId);
+// console.log(intervalId);
 
-// Recursive timeout
-setTimeout(function print(msg) {
-    console.log('Inside Timeout', msg);
-    setTimeout(print, 2000, 'hai');
-}, 3000, 'hello');
+// // Recursive timeout
+// setTimeout(function print(msg) {
+//     console.log('Inside Timeout', msg);
+//     setTimeout(print, 2000, 'hai');
+// }, 3000, 'hello');
+
+// Promises with setTimeout
+console.log('Before timeout promise');
+const custom = new Promise((resolve, reject) => {
+    console.log('Inside Promise');
+    setTimeout(() => {
+        console.log('Hello World !');
+        resolve();
+    }
+    , 2000);
+});
+
+custom
+.then(() => console.log('Resolved !'))
+.catch(() => console.log('Rejected !'))
+
+// Promise with setInterval
+console.log('Before interval promise');
+const custom2 = new Promise((resolve, reject) => {
+    console.log('Inside Promise');
+    setInterval(() => {
+        console.log('Hello World !');
+        reject();
+    }
+    , 2000);
+});
+
+custom
+.then(() => console.log('Resolved !'))
+.catch(() => console.log('Rejected !'))
