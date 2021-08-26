92f9deca6cc71da8befe570309c4f9215bb1fa9f
CauveryRaja <cauveryraja25@gmail.com>
2021-08-26 07:29:10 +0530

    Try duplicate identifier without declaration

--- misc/app.js
+++ misc/app.js
@@ -6,18 +6,29 @@ function foo() {
 
 console.log(foo()); // throws 'foo is not a function'
 
+// If variable is not defined, function will take preference. Since variables are hoisted first and then functions.
+function foo() {
+    console.log('Inside foo function...');
+}
+var foo; // Just declared not defined
+
+console.log(foo); // This prints the function
+
 let foo = 5;
 function foo() { // throws 'Identifier foo has already been declared'
     console.log('Inside foo function...');
 }
 */
 
-function foo() {
-    console.log('Inside foo function...');
-}
+// function foo() {
+//     console.log('Inside foo function...');
+// }
 
-function foo() {
-    console.log('Inside foo function 2...');
-}
+// function foo() {
+//     console.log('Inside foo function 2...');
+// }
 
-foo(); // last implementation will override previous ones
+// foo(); // last implementation will override previous ones
+
+
+
