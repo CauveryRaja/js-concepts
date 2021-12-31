4ccc2e448d47281d5df4238d1c07809c300add9f
CauveryRaja <cauveryraja25@gmail.com>
2021-12-31 06:45:30 +0530

    Listen multiple promises using all, allSettled

--- async js/app5.js
+++ async js/app5.js
@@ -8,7 +8,6 @@ const promise = new Promise((resolve, reject) => {
     console.log('Executing Timeout promise');
     setTimeout(() => resolve(5), 2000);
 });
-
 promise
 .then(data => console.log('Timeout Resolved value', data))
 .catch(err => console.log('Timeout Rejected value', err));
@@ -16,11 +15,27 @@ promise
 // Promise with setInterval
 const promise2 = new Promise((resolve, reject) => {
     console.log('Executing Interval promise');
-    setInterval(() => resolve(5), 2000);
+    // setInterval(() => resolve(5), 2000);
+    setTimeout(() => reject(10), 5000);
 });
-
 promise2
 .then(data => console.log('Interval Resolved value', data))
 .catch(err => console.log('Interval Rejected value', err));
+
+// Listening to multiple promises
+Promise.all([promise, promise2])
+.then(data => {
+    console.log('All promises resolved value', data)
+})
+.catch(err => {
+    console.log('All promises rejected value', err)
+});
+Promise.allSettled([promise, promise2])
+.then(data => {
+    console.log('All Settled promises resolved value', data)
+})
+.catch(err => {
+    console.log('All Settled promises rejected value', err)
+});
 
 console.log('Ending Async JS');
