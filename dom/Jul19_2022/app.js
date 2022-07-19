e5ea1f3ccae27b414cdd257498be5a1aca7d4e30
CauveryRaja <cauveryraja25@gmail.com>
2022-07-19 08:56:06 +0530

    Basic DOM injection

---
+++ dom/Jul19_2022/app.js
@@ -0,0 +1,5 @@
+const container = document.getElementsByClassName('container')[0];
+
+container.innerHTML = `<p>Hello world</p>`;
+container.innerText = '<p>Hello world</p>';
+container.textContent = '<p>Hello world</p>';
