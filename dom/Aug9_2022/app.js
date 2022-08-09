7d40d369459f6885eb95a7f13adc83ccef6e5d32
CauveryRaja <cauveryraja25@gmail.com>
2022-08-09 08:53:53 +0530

    Try getting dom element  values

---
+++ dom/Aug9_2022/app.js
@@ -0,0 +1,5 @@
+let boxElm = document.getElementsByClassName('container')[0];
+
+console.log('innerHtml', boxElm.innerHTML);
+console.log('innerText', boxElm.innerText);
+console.log('textContent', boxElm.textContent);
