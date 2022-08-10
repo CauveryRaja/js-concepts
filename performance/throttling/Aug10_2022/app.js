8559cc73ea2ac9a4b019e4234e13689ac9a823fa
CauveryRaja <cauveryraja25@gmail.com>
2022-08-10 09:26:47 +0530

    Implemented throttle function

---
+++ performance/throttling/Aug10_2022/app.js
@@ -0,0 +1,20 @@
+function throttle(callback, waitTime) {
+    let shouldTrigger = true;
+
+    return function() {
+        console.log('shouldTrigger', shouldTrigger);
+        if(shouldTrigger) {
+            callback();
+            shouldTrigger = false;
+        }
+        setTimeout(() => {
+            shouldTrigger = true;
+        }, waitTime);
+    }
+}
+
+function clickListener() {
+    console.log('Clicked...');
+}
+
+let throttledListener = throttle(clickListener, 5000);
