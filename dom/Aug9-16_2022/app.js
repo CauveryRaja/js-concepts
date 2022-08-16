f8de10b5357c3e7909fd0f41b6e86f40cd100cef
CauveryRaja <cauveryraja25@gmail.com>
2022-08-16 09:33:22 +0530

    Try other console methods to print

---
+++ dom/Aug9-16_2022/app.js
@@ -0,0 +1,9 @@
+let boxElm = document.getElementsByClassName('container')[0];
+
+console.log('innerHtml', boxElm.innerHTML);
+console.log('innerText', boxElm.innerText);
+console.log('textContent', boxElm.textContent);
+
+console.info('innerHtml', boxElm.innerHTML);
+console.info('innerText', boxElm.innerText);
+console.info('textContent', boxElm.textContent);
--- dom/Aug9_2022/app.js
+++
@@ -1,5 +0,0 @@
-let boxElm = document.getElementsByClassName('container')[0];
-
-console.log('innerHtml', boxElm.innerHTML);
-console.log('innerText', boxElm.innerText);
-console.log('textContent', boxElm.textContent);
