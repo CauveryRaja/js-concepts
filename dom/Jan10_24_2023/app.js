5c5f7c66f6be1f5306912ba6c0adc7f1cb4eb6b8
CauveryRaja <cauveryraja25@gmail.com>
2023-01-24 08:53:30 +0530

    Tried calling functions with string arguments too

--- dom/Jan10_24_2023/app.js
+++ dom/Jan10_24_2023/app.js
@@ -1,9 +1,9 @@
 const bodyElm = document.getElementsByTagName('body')[0];
 
 bodyElm.insertAdjacentHTML('beforeend', `
-    <div class="container" onClick="clickListener(${20})">Hello</div>
+    <div class="container" onClick="clickListener(${'20'}, 'hello')">Hello</div>
 `)
 
-function clickListener(value) {
-    console.log('clicked', value)
+function clickListener(value, msg) {
+    console.log('clicked', value, msg)
 }
